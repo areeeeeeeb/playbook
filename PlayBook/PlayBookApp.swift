2639f10c611dc16384cabd5ac468a0b4ae38d17b
areeeeeeeb <rasulareeb@gmail.com>
2023-11-16 00:21:38 -0500

    Initial Commit

---
+++ PlayBook/PlayBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayBookApp.swift
+//  PlayBook
+//
+//  Created by Areeb Rasul on 2023-11-16.
+//
+
+import SwiftUI
+
+@main
+struct PlayBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
